50c6365300224b1b2646552247a58529173f9f5c
matheusmelchiades <matheusmacielmelchiades@hotmail.com>
2022-07-11 02:39:16 -0300

    implemented filter in history

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import { Block } from "baseui/block";
 
 import "./app.css";
-import Topbar from "./components/topbar";
 import { HeadingSmall, LabelMedium } from "baseui/typography";
 import { FlexGrid, FlexGridItem } from "baseui/flex-grid";
 
@@ -11,9 +10,12 @@ import Select from "./components/select";
 import Button from "./components/button";
 import DatePicker from "./components/datepicker";
 import Table from "./components/table";
+import Topbar from "./components/topbar";
 import { convertDate } from "./components/utils";
+
 import api from "./services/api";
 import socket from "./services/socket";
+import { typeExchangeOptions } from "./config/application";
 
 const TYPE_DATA = {
   live_price: "Live Price",
@@ -61,14 +63,42 @@ const config = [
 export default function App() {
   const [data, setData] = useState([]);
 
-  const requestData = async () => {
+  const [filter, setFilter] = useState<{
+    type: { id: number; name: string; value: string }[];
+    fromDate: Date;
+    toDate: Date;
+  }>({} as any);
+
+  const requestData = async (params = {}) => {
     try {
-      const { data: allData = [] } = await api.get("/");
+      const { data: allData = [] } = await api.get("/", {
+        params,
+      });
 
       setData(allData);
     } catch (err) {
       setData([]);
     }
+  };
+
+  const handleUpdateFilter = (field: string, value: any) => {
+    setFilter((prev) => {
+      return Object.assign({}, prev, { [field]: value });
+    });
+  };
+
+  const handleRequestByFilter = () => {
+    const [typeOption] = filter.type as any[];
+    const fromDate = filter?.fromDate?.toISOString();
+    const toDate = filter?.toDate?.toISOString();
+
+    requestData({
+      fromDate,
+      toDate,
+      type: typeOption?.value,
+    })
+      .then()
+      .catch();
   };
 
   useEffect(() => {
@@ -99,28 +129,39 @@ export default function App() {
             alignItems="end"
           >
             <FlexGridItem>
-              <DatePicker label="From date" />
+              <DatePicker
+                label="From date"
+                onChange={(date) => handleUpdateFilter("fromDate", date)}
+                value={filter.fromDate}
+              />
             </FlexGridItem>
 
             <FlexGridItem>
-              <DatePicker label="To date" />
+              <DatePicker
+                label="To date"
+                onChange={(date) => handleUpdateFilter("toDate", date)}
+                value={filter.toDate}
+              />
             </FlexGridItem>
 
             <FlexGridItem>
               <Select
                 label="Type"
-                options={[
-                  { id: 0, label: "Live price", value: "live_price" },
-                  { id: 1, label: "Exchanged", value: "exchanged" },
-                ]}
+                options={typeExchangeOptions}
                 placeHolder="type"
-                labelKey="label"
+                labelKey="name"
                 valueKey="value"
+                onChange={({ value }) => handleUpdateFilter("type", value)}
+                value={filter.type}
               />
             </FlexGridItem>
 
             <FlexGridItem>
-              <Button label="Filter" color="secondary" />
+              <Button
+                label="Filter"
+                color="secondary"
+                onClick={handleRequestByFilter}
+              />
             </FlexGridItem>
           </FlexGrid>
 
