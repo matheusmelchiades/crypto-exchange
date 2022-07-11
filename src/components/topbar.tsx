4a2fc0efea9aa82bfc841214f2bff366731e036c
matheusmelchiades <matheusmacielmelchiades@hotmail.com>
2022-07-11 02:38:42 -0300

    integrate topbar with api and submmit form

--- src/components/topbar.tsx
+++ src/components/topbar.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 
 import { Block } from "baseui/block";
 import { FlexGrid, FlexGridItem } from "baseui/flex-grid";
@@ -8,6 +8,11 @@ import { HeadingLarge, LabelMedium } from "baseui/typography";
 import Select from "./select";
 import Input from "./input";
 import Button from "./button";
+import { cryptoCurrencies, fiatCurrencies } from "../config/application";
+import SelectOption from "./select-option";
+import api from "../services/api";
+
+import { useSnackbar } from "baseui/snackbar";
 
 const inputOverrides = {
   overrides: {
@@ -21,8 +26,39 @@ const inputOverrides = {
 };
 
 export default function Topbar() {
+  const { enqueue } = useSnackbar();
+  const [currencyFrom, setCurrencyFrom] = useState([]);
   const [amountFrom, setAmountFrom] = useState();
+  const [currencyTo, setCurrencyTo] = useState([]);
   const [amountTo, setAmountTo] = useState();
+
+  const requestNewExchange = useCallback(() => {
+    const [currencyFromOption = { name: "" }] = currencyFrom;
+    const [currencyToOption = { name: "" }] = currencyTo;
+
+    const payload = {
+      currencyFrom: currencyFromOption?.name,
+      amountFrom: Number(amountFrom || 0),
+      currencyTo: currencyToOption?.name,
+      amountTo: Number(amountTo || 0),
+    };
+
+    api
+      .post("/", payload)
+      .then(() => {
+        setCurrencyFrom([]);
+        setAmountFrom(undefined);
+        setCurrencyTo([]);
+        setAmountTo(undefined);
+
+        enqueue({
+          message: "Exchange submitted.",
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [currencyFrom, amountFrom, currencyTo, amountTo]);
 
   return (
     <Block
@@ -40,11 +76,21 @@ export default function Topbar() {
         alignItems="end"
       >
         <FlexGridItem>
-          <Select label="Currency from" options={[]} labelKey="" valueKey="" />
+          <Select
+            label="Currency from"
+            options={cryptoCurrencies}
+            labelKey="name"
+            valueKey="name"
+            getOptionLabel={(props) => <SelectOption {...props} />}
+            getValueLabel={(props) => <SelectOption {...props} />}
+            onChange={({ value }) => setCurrencyFrom(value)}
+            value={currencyFrom}
+          />
         </FlexGridItem>
 
         <FlexGridItem {...inputOverrides}>
           <Input
+            type="number"
             label="Amount from"
             placeHolder="amount"
             value={amountFrom}
@@ -69,11 +115,21 @@ export default function Topbar() {
         </FlexGridItem>
 
         <FlexGridItem>
-          <Select label="Currency To" options={[]} labelKey="" valueKey="" />
+          <Select
+            label="Currency To"
+            options={fiatCurrencies}
+            labelKey="name"
+            valueKey="name"
+            getOptionLabel={(props) => <SelectOption {...props} />}
+            getValueLabel={(props) => <SelectOption {...props} />}
+            onChange={({ value }) => setCurrencyTo(value)}
+            value={currencyTo}
+          />
         </FlexGridItem>
 
         <FlexGridItem {...inputOverrides}>
           <Input
+            type="number"
             label="Amount To"
             placeHolder="amount"
             value={amountTo}
@@ -84,7 +140,7 @@ export default function Topbar() {
         </FlexGridItem>
 
         <FlexGridItem>
-          <Button label="SAVE" />
+          <Button label="SAVE" onClick={requestNewExchange} />
         </FlexGridItem>
       </FlexGrid>
     </Block>
